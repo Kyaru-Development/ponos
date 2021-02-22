2428783f58b844c8464bba820c5d638731d2dbe4
Kotik44 <kyaru-dev@yandex.ru>
2021-02-23 02:17:09 +0300

    112121

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+ 
+
+namespace testprogram
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            DiscordWebhook.Send("fdfd");
+
+        }
+
+    }
+  
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("рттрћтћр")]
+[assembly: AssemblyDescription("тррттр")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("трћтрћтрћ")]
+[assembly: AssemblyProduct("тћртћртћр")]
+[assembly: AssemblyCopyright("ћтрћтрћтр")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("3b0e2d3d-3d66-42bb-8f9c-d6e188f359ae")]
+[assembly: AssemblyVersion("1.6.2.0")]
+[assembly: AssemblyFileVersion("1.6.2.0")]
